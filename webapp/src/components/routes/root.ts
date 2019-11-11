96a53f09033670c18465624c1d611b22a30f4795
OmegaPiece123 <qchen5249@bths.edu>
2019-11-11 14:11:47 -0500

    Register page done, and added Dashboard component

--- webapp/src/components/routes/root.ts
+++ webapp/src/components/routes/root.ts
@@ -1,12 +1,25 @@
 import { Route } from "../../models/Router";
 
 import SignIn from "../views/SignIn";
+import Register from "../views/Register";
+import Dashboard from './../views/Dashboard';
+
 
 const routes: Route[] = [
     {
         to: "/sign-in",
         exact: true,
         component: SignIn
+    },
+    {
+        to: "/sign-up",
+        exact: true,
+        component: Register
+    },
+    {
+        to: "/dashboard",
+        exact: true,
+        component: Dashboard
     }
 ];
 
---
+++ webapp/src/components/views/Dashboard.tsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+
+const Dashboard: React.FC  = () => {
+
+    return <div></div>
+}
+
+export default Dashboard;
