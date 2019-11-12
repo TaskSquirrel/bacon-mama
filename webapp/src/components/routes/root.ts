8c5e87dd5628598018fcb2e37d9e3eb0b8658d56
Ben Huang <ben.huang006@gmail.com>
2019-11-12 16:09:47 -0500

    Formatting

--- webapp/src/components/routes/root.ts
+++ webapp/src/components/routes/root.ts
@@ -3,8 +3,8 @@ import { Route } from "../../models/Router";
 import SignIn from "../views/SignIn";
 import ContentCreator from "../views/creator/ContentCreator";
 import Register from "../views/Register";
-import Dashboard from './../views/Dashboard';
-import CreateRecipe from './../views/CreateRecipe';
+import Dashboard from "./../views/Dashboard";
+import CreateRecipe from "../views/CreateRecipe";
 
 const routes: Route[] = [
     {
