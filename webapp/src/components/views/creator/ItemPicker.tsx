1a87d29d00259ba30cd3565b32a4b8c0d8178fc9
Ben Huang <ben.huang006@gmail.com>
2019-11-13 12:23:35 -0500

    Formatting

--- webapp/src/components/views/creator/ItemPicker.tsx
+++ webapp/src/components/views/creator/ItemPicker.tsx
@@ -1,33 +1,33 @@
 import React, { useContext, useState } from "react";
 
-import style from './ItemPicker.module.scss';
-
-import { ContentCreatorContext } from './ContentCreatorProvider';
+import { ContentCreatorContext } from "./ContentCreatorProvider";
 import { Item } from "../../../models/recipe";
 
-const ItemPicker: React.FC = () => {
+import styles from "./ItemPicker.module.scss";
 
+const ItemPicker: React.FC = () => {
     const {
         items,
     } = useContext(ContentCreatorContext);
 
-    const [pickedItem, setItem] = useState<Item[]>([{name:"hi"},{name:"hi"},{name:"hi"},{name:"hi"},{name:"hi"},{name:"hi"},{name:"hi"},{name:"hi"},{name:"hi"},{name:"hi"}]);
-
-    
+    const [pickedItem, setItem] = useState<Item[]>([]);
 
     return (
-        <div className={style.container}>
-            <button className={style.add}>
+        <div className={ styles.container }>
+            <button className={ styles.add }>
                 +
             </button>
-            <div className={style.items}>
-                {pickedItem.map((item) => {
-                    return(
-                        <div className={style.item}>
-                            {item.name}
+            <div className={ styles.items }>
+                { pickedItem.map((item) => {
+                    return (
+                        <div
+                            key={ item.name }
+                            className={ styles.item }
+                        >
+                            { item.name }
                         </div>
-                    )
-                })}
+                    );
+                }) }
             </div>
         </div>
     );
