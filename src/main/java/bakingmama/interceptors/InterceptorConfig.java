8deb8f17fc8e60c06fffc4c3afbd9cb8731c9cbb
yubrian123 <yubrian123@gmail.com>
2019-11-21 14:17:06 -0500

    Update InterceptorConfig.java
    added more routes

--- src/main/java/bakingmama/interceptors/InterceptorConfig.java
+++ src/main/java/bakingmama/interceptors/InterceptorConfig.java
@@ -9,6 +9,15 @@ public class InterceptorConfig extends WebMvcConfigurerAdapter {
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new TokenInterceptor()).addPathPatterns("/api/addRecipe");
 		registry.addInterceptor(new TokenInterceptor()).addPathPatterns("/api/editRecipe");
+		registry.addInterceptor(new TokenInterceptor()).addPathPatterns("/api/deleteRecipe");
+		registry.addInterceptor(new TokenInterceptor()).addPathPatterns("/api/addStep");
+		registry.addInterceptor(new TokenInterceptor()).addPathPatterns("/api/editStep");
+		registry.addInterceptor(new TokenInterceptor()).addPathPatterns("/api/deleteStep");
+		registry.addInterceptor(new TokenInterceptor()).addPathPatterns("/api/addIngredient");
+		registry.addInterceptor(new TokenInterceptor()).addPathPatterns("/api/editIngredient");
+		registry.addInterceptor(new TokenInterceptor()).addPathPatterns("/api/deleteIngredient");
+		registry.addInterceptor(new TokenInterceptor()).addPathPatterns("/api/validateID");
 	}
 }
