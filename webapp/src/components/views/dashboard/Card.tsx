c4fd9b9557b55eae41d43514c737371a66c2e4d4
Ben Huang <ben.huang006@gmail.com>
2019-12-15 23:59:15 -0500

    Lint all code

--- webapp/src/components/views/dashboard/Card.tsx
+++ webapp/src/components/views/dashboard/Card.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { Popup } from "semantic-ui-react";
 import classNames from "classnames";
 
 import Stack from "../../shared/Stack";
@@ -7,9 +8,6 @@ import ButtonBase from "../../controls/ButtonBase";
 import AuraButton from "../../controls/AuraButton";
 
 import styles from "./Card.module.scss";
-
-import { Popup } from 'semantic-ui-react';
-
 
 export interface CardProps extends React.HTMLProps<HTMLDivElement> {
     name: string;
@@ -54,22 +52,21 @@ const Card: React.FC<CardProps> = ({
 
         return (
             <Popup
-                size='mini'
-                content={status === "complete" ? "Done" : "Unfinished"}
-                trigger={(
+                size="mini"
+                content={ status === "complete" ? "Done" : "Unfinished" }
+                trigger={ (
                     <div
-                        className={classNames(
+                        className={ classNames(
                             styles.icon,
                             status === "complete" && styles.completed
-                        )}
+                        ) }
                     >
                         <i
-                            className={icon}
+                            className={ icon }
                         />
                     </div>
-                )}
+                ) }
             />
-
 
         );
     };
@@ -77,11 +74,11 @@ const Card: React.FC<CardProps> = ({
     const renderButton = () => {
         return (
             <AuraButton
-                className={classNames(
+                className={ classNames(
                     styles.button,
                     hovering && styles.hovering
-                )}
-                onClick={click}
+                ) }
+                onClick={ click }
             >
                 <i className="fas fa-times" />
             </AuraButton>
@@ -90,49 +87,49 @@ const Card: React.FC<CardProps> = ({
 
     return (
         <Stack
-            className={classNames(styles.card, className)}
-            onMouseOver={createHoveringSetter(true)}
-            onMouseOut={createHoveringSetter(false)}
+            className={ classNames(styles.card, className) }
+            onMouseOver={ createHoveringSetter(true) }
+            onMouseOut={ createHoveringSetter(false) }
         >
             <div
-                className={styles.name}
+                className={ styles.name }
             >
-                {name}
+                { name }
             </div>
             <div
-                className={styles.description}
+                className={ styles.description }
             >
-                {description}
+                { description }
             </div>
             <div
-                className={styles.actions}
+                className={ styles.actions }
             >
                 <div>
-                    {renderCompleted()}
+                    { renderCompleted() }
                 </div>
                 <div
-                    className={styles.edit}
+                    className={ styles.edit }
                 >
                     <Popup
-                        trigger={(
-                            <Link to={`/edit/${id}`}>
-                                <ButtonBase inverted clear className={styles.pen}>
+                        trigger={ (
+                            <Link to={ `/edit/${id}` }>
+                                <ButtonBase inverted clear className={ styles.pen }>
                                     <i className="fas fa-pen" />
                                 </ButtonBase>
                             </Link>
-                        )}
+                        ) }
                         size="mini"
                         content="Edit"
                     />
                     <Popup
-                        className={styles.popup}
-                        trigger={(
-                            <Link to={`/play/${id}`}>
-                                <ButtonBase inverted clear className={styles.play}>
+                        className={ styles.popup }
+                        trigger={ (
+                            <Link to={ `/play/${id}` }>
+                                <ButtonBase inverted clear className={ styles.play }>
                                     <i className="fas fa-play" />
                                 </ButtonBase>
                             </Link>
-                        )}
+                        ) }
 
                         size="mini"
                         content="Play"
@@ -140,7 +137,7 @@ const Card: React.FC<CardProps> = ({
 
                 </div>
             </div>
-            {!isStudent && renderButton()}
+            { !isStudent && renderButton() }
         </Stack>
     );
 };
